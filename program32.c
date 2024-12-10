e24963da9bc4e05ddd80ff8c29dab981f861cd7d
sughamni123 <gsu6204@gmail.com>
2024-12-10 15:50:47 +0530

    day 5
    add files via upload

---
+++ program32.c
@@ -0,0 +1,19 @@
+//scenario based program //
+#include<stdio.h>
+int main()
+{
+    int N=10,k=5,num;
+    scanf("%d",&num);
+    if(num>=1&&num<=10&& n-num>=0)
+    {
+    printf("The number of candies sold:%d\n ",num);
+    
+    printf("The number of candies left:%d ",n-num>k?n-num:10);
+    }
+else
+{
+    printf("Invalid Input\n");
+    printf("Number of candies left:%d",n);
+}
+return 0;
+}
